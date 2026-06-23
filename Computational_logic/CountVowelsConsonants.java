3e538956e8c30b331417aee8b620da5af1c11077
AnshikaAgarwal123 <Agarwalanshika246@gmail.com>
2026-06-23 21:26:28 +0530

    Create CountVowelsConsonants.java

---
+++ Computational_logic/CountVowelsConsonants.java
@@ -0,0 +1,25 @@
+public class CountVowelsConsonants {
+    public static void main(String[] args) {
+        String str = "GitHub Streak";
+        int vowels = 0;
+        int consonants = 0;
+
+        str = str.toLowerCase();
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("String: GitHub Streak");
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
