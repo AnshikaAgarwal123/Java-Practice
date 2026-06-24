a0ec7a8b45c8093f4a694a62870de3a84d6dc7dd
AnshikaAgarwal123 <Agarwalanshika246@gmail.com>
2026-06-24 19:37:19 +0530

    Create Solid_Square_Pattern.java

---
+++ Pattern Printing/Solid_Square_Pattern.java
@@ -0,0 +1,19 @@
+//Input-4
+//Output-
+// * * * * 
+// * * * * 
+// * * * * 
+// * * * * 
+import java.util.*;
+public class Solid_Square_Pattern {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
