3916b610b06483c07a3cbbb3764816e40e995256
AnshikaAgarwal123 <Agarwalanshika246@gmail.com>
2026-06-27 22:23:50 +0530

    Create Numbered_Pattern2.java

---
+++ Pattern Printing/Numbered_Pattern2.java
@@ -0,0 +1,22 @@
+//Input-4
+//Output-
+//    1
+//   222
+//  33333
+// 4444444
+import java.util.*;
+public class Numbered_Pattern2 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<(2*i)-1; j++){
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
