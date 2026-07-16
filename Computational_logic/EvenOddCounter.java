01902627a0495908c69b29b5e84e0f1fb193b97d
AnshikaAgarwal123 <agarwalanshika246@gmail.com>
2026-07-16 21:00:17 +0530

    Create EvenOddCounter.java

---
+++ Computational_logic/EvenOddCounter.java
@@ -0,0 +1,19 @@
+public class EvenOddCounter {
+    public static void main(String[] args) {
+        int[] arr = {1, 5, 8, 12, 15, 20, 23};
+        
+        int evenCount = 0;
+        int oddCount = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                evenCount++; 
+            } else {
+                oddCount++;  
+            }
+        }
+
+        System.out.println("Even numbers count: " + evenCount);
+        System.out.println("Odd numbers count: " + oddCount);
+    }
+}
