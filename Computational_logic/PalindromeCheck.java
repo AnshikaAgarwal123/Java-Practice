bc80ecde1faf1396d294eebf0ac40a8591b7c62d
AnshikaAgarwal123 <agarwalanshika246@gmail.com>
2026-07-31 23:35:45 +0530

    Create PalindromeCheck.java

---
+++ Computational_logic/PalindromeCheck.java
@@ -0,0 +1,24 @@
+public class PalindromeCheck {
+    public static void main(String[] args) {
+        String word = "radar".toLowerCase();
+        
+        boolean isPalindrome = true;
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("\"" + word + "\" is a palindrome.");
+        } else {
+            System.out.println("\"" + word + "\" is NOT a palindrome.");
+        }
+    }
+}
